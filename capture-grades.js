bfaf05136d17ae76713b84a88f9c2b6ed3ceb921
gncb <gncb@users.noreply.github.com>
2024-02-07 04:46:22 +0000

    deploy: 8827c1a3fc7ffe18606a4351e91da74deaf45a2e

---
+++ capture-grades.js
@@ -0,0 +1,8 @@
+const page = await browser.newPage();
+await page.goto('https://www.jiritsu-red.jp/contact/');
+const gradeOptionNodes = await page.$$eval(
+    "select#gakunen > option", 
+    options => options.map(option => option.textContent)
+);
+gradeOptionNodes.shift();
+return gradeOptionNodes
