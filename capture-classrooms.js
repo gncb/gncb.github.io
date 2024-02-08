9f61d9efdc4150b3cdd51120d0c2cbd9c6b80e06
shimazu hirofumi <hirofumi-shimazu@J0061.local>
2024-02-08 14:33:01 +0900

    change code 0208

--- capture-classrooms.js
+++ capture-classrooms.js
@@ -49,7 +49,7 @@ for ( i = 0; i < list_pref.length; i++) {
         // 教室のオプションを取得
         classroomOptionNodes = await page.$$eval(
             "select#classroom > option", 
-            options => options.map(option => option.value)
+            options => options.map(option => { id: option.value})
         );
         console.log(classroomOptionNodes);
         master[list_pref[i]][master[list_pref[i]][j]] = classroomOptionNodes.map(option => {
